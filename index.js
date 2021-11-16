0c190bcf53123ac5454a2b148d5bed8551eee99d
damian <damiankohut@gmail.com>
2021-11-16 16:08:48 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //write your code here
+function replaceWithYerr(array, startIndex = 0, deleteCount = array.length){
+    let newArray = [...array] //make a copy of the OG array
+
+    for(let i = startIndex; i < deleteCount + startIndex; i++){
+        newArray[i] = 'yerr'
+    }
+    
+    return newArray
+}
+const months = ['Jan', 'February', 'March', 'April', 'May', 'June'];
+console.log(replaceWithYerr(months, 1, 3))
